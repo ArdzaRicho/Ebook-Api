94dc0c6ff7820a3a6bb92128f95f045891792a18
ArdzaRicho <bullsdog32739@gmail.com>
2022-11-01 08:24:34 +0700

    migration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controlles;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controllers
+{
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    //return $request->user();
+//});
+
+Route::get('api',function(){
+    return["Ardzaricho" => "biasa"];
 });
+
+//Route::('halocontroller',[HeloController::class, 'index']);
+
+Route::resource('halo', HeloController::class);
+
+Route::resource('siswa', SiswaController::class, ['except' => ['create', 'edit']]);
+
+Route::get('siswas/{id}', [SiswaController::class, 'update']);
+
+// Route::get('books', [BookController::class, 'index']);
+// Route::get('books/{id}', [BookController::class, 'show']);
+// Route::get('books', [BookController::class, 'store']);
+// Route::get('books/{id}', [BookController::class, 'update']);
+// Route::get('books/{id}', [AuthController::class, 'destroy']);
+
+Route::resource('books', BookController::class);
